10463434e26111d5875c5c79cbd3c6294c0eed9e
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-15 01:35:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = () => {
+    const some1 = 2000
+    const some2 = 100
+    return some1 / some2
+}
+
+const square = (num) => num*num 
+
+
+const add = (num1, num2) => {
+    return num1+num2
+
+}
